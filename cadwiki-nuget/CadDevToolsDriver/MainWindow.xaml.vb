9bd231b66a52e0c825b9e1e1f64b7a40d74c79d7
cadwiki <99513485+cadwiki@users.noreply.github.com>
2022-09-24 07:35:51 -0700

    hide CadDevToolsDriver

--- cadwiki-nuget/CadDevToolsDriver/MainWindow.xaml.vb
+++ cadwiki-nuget/CadDevToolsDriver/MainWindow.xaml.vb
@@ -3,6 +3,7 @@ Class MainWindow
     Public Sub New()
         ' This call is required by the designer.
         InitializeComponent()
+        Me.Hide()
         Dim window As Window = New cadwiki.CadDevTools.MainWindow()
         window.Show()
     End Sub
