278cde81b79e7bfa534a13bb181fac2b3f363450
cadwiki <99513485+cadwiki@users.noreply.github.com>
2022-10-28 15:46:32 -0700

    update version numbers to 1.0.5.0

--- cadwiki-nuget/CadDevToolsDriver/My Project/AssemblyInfo.vb
+++ cadwiki-nuget/CadDevToolsDriver/My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Reflection
+Imports System.Reflection
 Imports System.Runtime.InteropServices
 
 ' General Information about an assembly is controlled through the following
@@ -50,5 +50,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.4.0")>
-<Assembly: AssemblyFileVersion("1.0.4.0")>
+<Assembly: AssemblyVersion("1.0.5.0")>
+<Assembly: AssemblyFileVersion("1.0.5.0")>
