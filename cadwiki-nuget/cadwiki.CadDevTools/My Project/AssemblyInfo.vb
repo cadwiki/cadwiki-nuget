3885160c54445d13c3eb1e661f3318a766dd3220
cadwiki <99513485+cadwiki@users.noreply.github.com>
2022-11-01 20:40:17 -0700

    update version numbers

--- cadwiki-nuget/cadwiki.CadDevTools/My Project/AssemblyInfo.vb
+++ cadwiki-nuget/cadwiki.CadDevTools/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.10.2033")>
-<Assembly: AssemblyFileVersion("1.0.10.2033")>
+<Assembly: AssemblyVersion("1.0.11.2039")>
+<Assembly: AssemblyFileVersion("1.0.11.2039")>
