43e9277c73761c72ca0fe6288a31b8a454117d3c
cadwiki <cadwiki.net@gmail.com>
2022-11-20 07:52:47 -0800

    update version to 1.0.12.726

--- cadwiki-nuget/cadwiki.NetUtils/My Project/AssemblyInfo.vb
+++ cadwiki-nuget/cadwiki.NetUtils/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.11.2039")>
-<Assembly: AssemblyFileVersion("1.0.11.2039")>
+<Assembly: AssemblyVersion("1.0.12.726")>
+<Assembly: AssemblyFileVersion("1.0.12.726")>
