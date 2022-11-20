737c13c29c21f066a315cdf74e0375c97561644e
cadwiki <cadwiki.net@gmail.com>
2022-11-20 07:23:14 -0800

    add error handling to DllReloadClickCommandHandler Execute

--- cadwiki-nuget/cadwiki.DllReloader/AutoCAD/UiRibbon/Buttons/DllReloadClickCommandHandler.vb
+++ cadwiki-nuget/cadwiki.DllReloader/AutoCAD/UiRibbon/Buttons/DllReloadClickCommandHandler.vb
@@ -19,24 +19,35 @@ Namespace AutoCAD.UiRibbon.Buttons
 
         Public Sub Execute(ByVal parameter As Object) Implements System.Windows.Input.ICommand.Execute
             Dim doc As Document = Application.DocumentManager.MdiActiveDocument
-
             doc.Editor.WriteMessage(vbLf & "DllReloadClickCommandHandler started..")
 
-            If TypeOf parameter Is RibbonButton Then
-                Dim button As RibbonButton = TryCast(parameter, RibbonButton)
-                Dim uiRouter As UiRouter = TryCast(button.CommandParameter, UiRouter)
-                If (doc IsNot Nothing) Then
-                    Dim netReloader As AutoCADAppDomainDllReloader = uiRouter.NetReloader
-                    Dim iExtensionAppAssembly As Assembly = uiRouter.IExtensionAppAssembly
-                    Dim userInputDllPath As String = netReloader.UserInputGetDllPath()
-                    If String.IsNullOrEmpty(userInputDllPath) Then
+            Try
+                If TypeOf parameter Is RibbonButton Then
+                    Dim button As RibbonButton = TryCast(parameter, RibbonButton)
+                    Dim uiRouter As UiRouter = TryCast(button.CommandParameter, UiRouter)
+                    If (uiRouter Is Nothing) Then
+                        Dim window As cadwiki.WpfUi.Templates.WindowAutoCADException =
+                            New cadwiki.WpfUi.Templates.WindowAutoCADException(New Exception("Not able to cast parameter to UiRouter object."))
+                        window.Show()
                         Return
-                    Else
-                        netReloader.ReloadDll(doc, iExtensionAppAssembly, userInputDllPath)
                     End If
-
+                    If (doc IsNot Nothing) Then
+                        Dim netReloader As AutoCADAppDomainDllReloader = uiRouter.NetReloader
+                        Dim iExtensionAppAssembly As Assembly = uiRouter.IExtensionAppAssembly
+                        Dim userInputDllPath As String = netReloader.UserInputGetDllPath()
+                        If String.IsNullOrEmpty(userInputDllPath) Then
+                            Return
+                        Else
+                            netReloader.ReloadDll(doc, iExtensionAppAssembly, userInputDllPath)
+                        End If
+                    End If
                 End If
-            End If
+            Catch ex As Exception
+                Dim window As cadwiki.WpfUi.Templates.WindowAutoCADException =
+                    New cadwiki.WpfUi.Templates.WindowAutoCADException(New Exception("Not able to cast parameter to UiRouter object."))
+                window.Show()
+            End Try
+
         End Sub
 
 
