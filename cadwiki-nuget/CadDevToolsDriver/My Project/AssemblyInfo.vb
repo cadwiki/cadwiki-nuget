ad79583b6913b640f9b14e0c8cdf7a174ee0a0af
cadwiki <cadwiki.net@gmail.com>
2022-12-12 15:25:01 -0800

    update version to 1.0.16.1525

--- cadwiki-nuget/CadDevToolsDriver/My Project/AssemblyInfo.vb
+++ cadwiki-nuget/CadDevToolsDriver/My Project/AssemblyInfo.vb
@@ -50,5 +50,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.15.1423")>
-<Assembly: AssemblyFileVersion("1.0.15.1423")>
+<Assembly: AssemblyVersion("1.0.16.1525")>
+<Assembly: AssemblyFileVersion("1.0.16.1525")>
