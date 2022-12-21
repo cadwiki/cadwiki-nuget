0eac771d3c6021ca98dc44c9125179945b165d8e
cadwiki <cadwiki.net@gmail.com>
2022-12-20 17:40:23 -0800

    add HandleBars package for parameterizing html templates

---
+++ cadwiki-nuget/UnitTests/cadwiki.NUnitTestRunner/TestsForHtmlCreator.vb
@@ -0,0 +1,14 @@
+﻿Imports cadwiki.NUnitTestRunner
+Imports cadwiki.NUnitTestRunner.Creators
+Imports cadwiki.NUnitTestRunner.TestEvidence
+
+<TestClass()> Public Class TestsForHtmlCreator
+
+
+
+    <TestMethod()> Public Sub Test_CreateReport_ShouldPass()
+        Dim htmlCreator As New HtmlCreator
+        htmlCreator.ParameterizeReportTemplate()
+    End Sub
+
+End Class
---
+++ cadwiki-nuget/cadwiki.NUnitTestRunner/Creators/HtmlCreator.vb
@@ -0,0 +1,16 @@
+﻿Imports System.IO
+Imports System.Reflection
+Imports HandlebarsDotNet
+
+Public Class HtmlCreator
+    Public Sub ParameterizeReportTemplate()
+        Dim assembly As Assembly = System.Reflection.Assembly.GetExecutingAssembly()
+        Dim templateString As String = NetUtils.AssemblyUtils.ReadEmbeddedResourceToString(assembly, "HtmlTemplateTestSuiteReport.cshtml")
+        If (templateString IsNot Nothing) Then
+            Dim template As HandlebarsDotNet.HandlebarsTemplate(Of Object, Object) = Handlebars.Compile(templateString)
+            Dim reportModel As New HtmlModelTestSuiteReport
+            reportModel.BannerImagePath = "test"
+            Dim result As Object = template(reportModel)
+        End If
+    End Sub
+End Class
