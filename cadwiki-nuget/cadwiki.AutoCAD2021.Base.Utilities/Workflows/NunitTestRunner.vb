45122474f43f8bf097a24de60557c0fd21746ccf
cadwiki <99513485+cadwiki@users.noreply.github.com>
2023-04-11 20:42:28 -0700

    Create NunitTestRunner.vb

---
+++ cadwiki-nuget/cadwiki.AutoCAD2021.Base.Utilities/Workflows/NunitTestRunner.vb
@@ -0,0 +1,27 @@
+﻿Option Strict On
+Option Infer Off
+Option Explicit On
+
+Imports Autodesk.AutoCAD.ApplicationServices
+Imports Autodesk.AutoCAD.EditorInput
+Imports cadwiki.NUnitTestRunner.Results
+Imports cadwiki.NUnitTestRunner.UI
+
+Namespace Workflows
+    Public Class NunitTestRunner
+        Public Async Function Run(ByVal regressionTestTypes As Type()) As Task
+            Dim doc As Document = Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument
+            Dim ed As Editor = doc.Editor
+            Try
+                Dim results As ObservableTestSuiteResults = New cadwiki.NUnitTestRunner.Results.ObservableTestSuiteResults()
+                Dim driver As WpfDriver = New cadwiki.NUnitTestRunner.UI.WpfDriver(results, regressionTestTypes)
+                Dim window As WindowTestRunner = driver.GetWindow()
+                ' https://forums.autodesk.com/t5/net/how-to-set-a-focus-to-autocad-main-window-from-my-form-of-c-net/td-p/4680059
+                Autodesk.AutoCAD.ApplicationServices.Core.Application.ShowModelessWindow(window)
+                Await driver.ExecuteTestsAsync()
+            Catch ex As Exception
+                ed.WriteMessage(Environment.NewLine.ToString() & "Exception: " + ex.Message)
+            End Try
+        End Function
+    End Class
+End Namespace
