a6a22e3549a0b4636b96d8f5bbedb9639118d711
cadwiki <cadwiki.net@gmail.com>
2024-06-23 22:20:19 -0700

    update to 2.0.0.0

--- cadwiki-nuget/CadDevToolsDriver/My Project/AssemblyInfo.cs
+++ cadwiki-nuget/CadDevToolsDriver/My Project/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")>
 
-[assembly: AssemblyVersion("1.1.7.610")]
-[assembly: AssemblyFileVersion("1.1.7.610")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
