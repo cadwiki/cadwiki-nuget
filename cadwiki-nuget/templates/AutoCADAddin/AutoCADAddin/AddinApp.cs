b0aeff8e38355e779737f961000c8cb596d826bb
cadwiki <99513485+cadwiki@users.noreply.github.com>
2024-08-07 21:58:42 -0700

    create template project

---
+++ cadwiki-nuget/templates/AutoCADAddin/AutoCADAddin/AddinApp.cs
@@ -0,0 +1,58 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Exception = System.Exception;
+
+namespace AutoCADAddin
+{
+    public class AddinApp : IExtensionApplication
+    {
+
+        public void Initialize()
+        {
+            try
+            {
+                var dll = Assembly.GetExecutingAssembly();
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.Show(ex);
+            }
+
+        }
+
+        public void Terminate()
+        {
+
+        }
+
+        public static string GetLogsFolder()
+        {
+            var dllPath = Assembly.GetExecutingAssembly().Location;
+            var dllName = Assembly.GetExecutingAssembly().GetName().Name;
+            var tempFolder = System.IO.Path.GetTempPath() + "\\" + dllName  + "\\logs";
+            
+            if (dllPath != null)
+            {
+                var dllFolder = System.IO.Path.GetDirectoryName(dllPath);
+                var logs = dllFolder + "\\" + "\\logs";
+                if (!System.IO.Directory.Exists(logs))
+                {
+                    System.IO.Directory.CreateDirectory(logs);
+                }
+                return logs;
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(tempFolder);
+            }
+            
+
+            return tempFolder;
+        }
+    }
+}
---
+++ cadwiki-nuget/templates/AutoCADAddin/AutoCADAddin/ErrorHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AutoCADAddin
+{
+    public class ErrorHandler
+    {
+        public static void Show(Exception ex)
+        {
+            System.Windows.Forms.MessageBox.Show(
+                ex.Message, 
+                "Error", 
+                System.Windows.Forms.MessageBoxButtons.OK, 
+                System.Windows.Forms.MessageBoxIcon.Error);  
+        }
+    }
+}
