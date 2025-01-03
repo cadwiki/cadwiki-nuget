e0b08e752d5d45409a403afeb0c9d5c63e1da52c
cadwiki <99513485+cadwiki@users.noreply.github.com>
2025-01-03 06:39:12 -0800

    chore: update version number

--- cadwiki-nuget/cadwiki.AdminOps/Properties/AssemblyInfo.cs
+++ cadwiki-nuget/cadwiki.AdminOps/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("3.0.0.1")]
-[assembly: AssemblyFileVersion("3.0.0.1")]
+[assembly: AssemblyVersion("3.0.0.2")]
+[assembly: AssemblyFileVersion("3.0.0.2")]
