ee37c371a45e59a1203d7a4c0067cbddc479ab22
cadwiki <99513485+cadwiki@users.noreply.github.com>
2025-12-12 19:11:01 -0800

    feat(AC): add DocProcessor

---
+++ cadwiki-nuget/cadwiki.AC/DocProcessor/SdiMode.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.AutoCAD.ApplicationServices.Core;
+using System;
+
+namespace cadwiki.AC.DocProcessor
+{
+    public enum SdiMode
+    {
+        SingleDocument,
+        MultiDocument
+    }
+
+    public class SdiModeChecker
+    {
+        public static SdiMode GetSdiMode()
+        {
+            var sdi_mode = Convert.ToInt32(Application.GetSystemVariable("SDI"));
+            if (sdi_mode == 1)
+            {
+                return SdiMode.SingleDocument;
+            }
+            else
+            {
+                return SdiMode.MultiDocument;
+            }
+        }
+    }
+}
